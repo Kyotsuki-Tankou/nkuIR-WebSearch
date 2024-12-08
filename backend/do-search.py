4712db75175bff4b6ba1805d643cf68837e3b7b1
Kyotsuki Tankou <KyotsukiTankou@outlook.com>
2024-12-09 01:00:48 +0800

    complete do_search.py

--- backend/do-search.py
+++ backend/do-search.py
@@ -1,17 +1,12 @@
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
-import os
-import networkx as nx
-from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from collections import Counter
-import jieba
 import re
-import csv
 import numpy as np
 from flask import Flask,request,jsonify
 
-from backend.query_gen import get_index,gen_query
+from query_gen import get_index,gen_query
 
 index_name,index_body=get_index()
 
@@ -40,9 +35,12 @@ def conduct_query(
     query_len=len(results)
     query_cnt=0
     query_list={}
+    result_list=[]
     regex_patterns=[re.compile(regex_word) for regex_word in query_word_regex]
     
     for hit in results:
+        if query_cnt>=query_size:
+            break
         title=hit['_source']['title']
         content=hit['_source']['content']
         url=hit['_source']['url']
@@ -50,15 +48,17 @@ def conduct_query(
         #去除html标签
         soup=BeautifulSoup(content,features="lxml")
         text_content=soup.get_text()
-        
-        match_found=any(pattern.search(title) or pattern.search(url) or pattern.search(text_content) for pattern in regex_patterns)
-        if not match_found:
+
+        if len(regex_patterns)>0:
+            match_found=any(pattern.search(title) or pattern.search(url) or pattern.search(text_content) for pattern in regex_patterns)
+            if not match_found:
+                continue
+        if (query_domain is not None) and (query_domain not in url):
             continue
+        
         query_cnt+=1
-        if query_cnt>query_size:
-            continue
-        
         cleaned_content=' '.join(text_content.split()) #去除所有空字符
+        print("-"*50)
         print(f"Title:{title}")
         print(f'url: {url}')
         print(f'content: {cleaned_content[:250]}...')
@@ -77,13 +77,18 @@ def conduct_query(
             else:
                 query_list[token]=count
             print(f"{token}:{count}")
-            
         print("-"*50)
-        
+        result_list.append(
+            {
+                'title':title,
+                'utl':url,
+                'content':cleaned_content[:250]
+            }
+        )
+    
     while len(results):
-        # if query_len>=query_size:
-        #     break
-        # print(f"length of results is:{len(results)}")
+        if query_cnt>=query_size:
+            break
         response=es.scroll(scroll_id=scroll_id,scroll='5m')
         scroll_id=response['_scroll_id']
         results=response['hits']['hits']
@@ -91,30 +96,24 @@ def conduct_query(
         # result_list+=results
         
         for hit in results:
+            if query_cnt>query_size:
+                break
             title=hit['_source']['title']
             content=hit['_source']['content']
             url=hit['_source']['url']
             
-            #去除html标签
             soup=BeautifulSoup(content,features="lxml")
             text_content=soup.get_text()
             
-            #检查title， url和text_content中是否有匹配regex_patterns的内容，对于每个正则语句，三者有一个满足即可
-            # match_found=False
-            # for pattern in regex_patterns:
-            #     if not pattern.search(title) and not pattern.search(url) and not pattern.search(text_content):
-            #         match_found=True
-            #         break
-                
-            # if not match_found:
-            #     continue
-            match_found=any(pattern.search(title) or pattern.search(url) or pattern.search(text_content) for pattern in regex_patterns)
-            if not match_found:
-                continue
-            query_cnt+=1
-            if query_cnt>query_size:
+            if len(regex_patterns)>0:
+                match_found=any(pattern.search(title) or pattern.search(url) or pattern.search(text_content) for pattern in regex_patterns)
+                if not match_found:
+                    continue
+            
+            if (query_domain is not None) and (query_domain not in url):
                 continue
             
+            query_cnt+=1
             cleaned_content=' '.join(text_content.split()) #去除所有空字符
             print(f"Title:{title}")
             print(f'url: {url}')
@@ -135,6 +134,33 @@ def conduct_query(
                 else:
                     query_list[token]=count
                 print(f"{token}:{count}")
+            result_list.append(
+                {
+                    'title':title,
+                    'utl':url,
+                    'content':cleaned_content[:250]
+                }
+            )
                 
     # return query_cnt,query_list,result_list
-    return query_cnt,query_list
+    return query_cnt,query_list,result_list
+
+if __name__=="__main__":
+    query_word_term=['程明明']
+    query_word_phrase=[]
+    query_word_regex=[]
+    query_domain='weekly.nankai.edu.cn'
+    frequent_token=['华为']
+    
+    es = Elasticsearch(
+        hosts=["http://localhost:9200"],
+        http_auth=('elastic', '123456')
+    )
+    
+    query_cnt,query_list,result_list=conduct_query(query_word_term=query_word_term,
+    query_word_phrase=query_word_phrase,query_word_regex=query_word_regex,query_domain=query_domain,frequent_token=['华为'],
+    es=es,index_name=index_name,fields=['title','anchor','content','url'],query_size=5)
+    
+    print(query_cnt)
+    print(query_list)
+    print(result_list)
